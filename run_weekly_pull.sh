cdadf3bb90f9cb45f6da1d8d348fec61c4634efd
Alain Soto <134567742+AlainS7@users.noreply.github.com>
2025-05-25 01:29:33 -0400

    added data management

---
+++ run_weekly_pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Quick test script for weekly data pull
+cd "/Users/alainsoto/IdeaProjects/Hot Durham"
+"/Users/alainsoto/IdeaProjects/Hot Durham/.venv/bin/python3" "/Users/alainsoto/IdeaProjects/Hot Durham/scripts/automated_data_pull.py" --weekly
